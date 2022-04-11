7bd944251e5fc2318461c981327e430587a7daaf
Sandrine <sandrine.herve88@gmail.com>
2022-04-11 23:41:17 +0200

    affichage posts ~

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,8 @@ app.use(helmet()); //Protéger les en-têtes http
 
 //Accès à l'api
 app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Opener-Policy','cross-origin');
+    res.setHeader('Cross-Origin-Resource-Policy','cross-origin');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
--- groupomania-app/src/components/mur.vue
+++ groupomania-app/src/components/mur.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class='header' id='header'>
-    <h1> Bienvenue sur votre réseau social {{name}} ! </h1>
-<!-- bouton de déconnexion -->
-    <button v-on:click.prevent='logout()' type="button" class="btn btn-secondary " > Se déconnecter !</button>
-<!-- bouton de suppression de compte -->
-    <button v-on:click.prevent='deleteCount(userId)' type='button' class='btn btn-danger'> supprimer votre compte !</button>
+  <div>
+
+    <div class='header' id='header'>
+      <h1> Bienvenue sur votre réseau social {{name}} ! </h1>
+      <!-- bouton de déconnexion -->
+      <button v-on:click.prevent='logout()' type="button" class="btn btn-secondary " > Se déconnecter !</button>
+      <!-- bouton de suppression de compte -->
+      <button v-on:click.prevent='deleteCount(userId)' type='button' class='btn btn-danger'> supprimer votre compte !</button>
+    </div>
+  
+    <!-- Afficher tous les posts avec les images -->
+    <div class='posts' id='posts'>
+      <h2>Voici les derniers posts de notre communauté :</h2>
+      <!-- les posts -->
+      <div class="posted">
+        <div id="postdiv" class="post"  v-for="post in posts" :key="post.id">
+          <!-- v-for="message in messages.slice().reverse()" :key="message.id" -->
+          <div class="col-10 offset-1 bg-light rounded shadow-sm mb-3">
+            <h3 class="pt-3 mb-0">{{ post.title }}</h3>
+            <small class="text-start pe-0 text-secondary" >publié par <span class="fw-bold">{{ post.userId }}</span></small>
+            <p class="pt-3 mb-1">{{ post.content }}</p>
+            <div>
+              <img :src=" post.media  " alt= "image du post " />
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- fin les posts -->
+    </div>
+
   </div>
+
 </template>
 
 
@@ -18,6 +43,12 @@ export default {
     return {
       name:'',
       userId: JSON.parse(this.$localStorage.get('userId')),
+      posts:[],
+           title:'',
+           content:'',
+           dateAdd:'',
+           id:'',
+           media:'',
       
     } 
   },
@@ -25,6 +56,23 @@ export default {
     if (localStorage.name) {
       this.name = localStorage.name;
     }
+    axios.get('http://localhost:3000/api/posts/getAll',
+     {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": 'Bearer ' + this.token
+        }
+      }) 
+      .then(res => { console.log(res.data)
+        })
+      .catch(error => console.log(error));
+      
+            
+    
+  },
+  created() {
+     
   },
   watch: {
     username(newName) {
@@ -65,6 +113,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+ul {
+  list-style-type : none;
+}
 
 </style>
