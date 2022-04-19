29a43df0f009c01ce912b4484cf596eb5509e122
Sandrine <sandrine.herve88@gmail.com>
2022-04-19 16:06:16 +0200

    MIJ

--- backend/server.js
+++ backend/server.js
@@ -47,15 +47,31 @@ server.on('listening', () => {
 });
 
 // Connexion a la base de donnée MySQL avec SEquelize
-// const sequelize = new Sequelize("database_development", "root", "1MySQLCompteDauphin!", {
-//     dialect: "mysql",
-//     host: "localhost"
+const sequelize = new Sequelize("database_development", "root", "1MySQLCompteDauphin!", {
+    dialect: "mysql",
+    host: "localhost"
+});
+
+try {
+    sequelize.authenticate();
+    console.log('Connecté à la base de données MySQL!');
+  } catch (error) {
+    console.error('Impossible de se connecter, erreur suivante :', error);
+  }
+server.listen(port);
+
+
+// server.on('error', errorHandler);
+// server.on('listening', () => {
+//   const address = server.address();
+//   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+//   console.log('Listening on ' + bind);
 // });
 
-// try {
-//     sequelize.authenticate();
-//     console.log('Connecté à la base de données MySQL!');
-//   } catch (error) {
-//     console.error('Impossible de se connecter, erreur suivante :', error);
-//   }
-server.listen(port);
+// const db = require('./models');
+
+// db.sequelize.sync().then(() => {
+//     app.listen(port, () => {
+//         console.log("Server running on port "+port);
+//     });
+// });
